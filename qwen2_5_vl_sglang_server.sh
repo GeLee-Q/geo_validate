5796fee075c12ad697822f95276d5cf5cfb4c695
Chayenne <zhaochen20@outlook.com>
2025-04-09 11:46:33 -0700

    Update qwen2_5_vl_sglang_server.sh

--- qwen2_5_vl_sglang_server.sh
+++ qwen2_5_vl_sglang_server.sh
@@ -1,7 +1,7 @@
 python -m sglang.launch_server \
     --model-path Qwen/Qwen2.5-VL-7B-Instruct \
     --host 0.0.0.0 \
-    --port 30000 \
+    --port 30201 \
     --disable-radix-cache \
     --chunked-prefill-size -1 \
     --chat-template qwen2-vl \
